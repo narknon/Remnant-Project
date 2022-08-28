d8c23b801e07705853761227f7fda3719df2e50e
Narknon <narknon0@gmail.com>
2022-08-28 18:41:27 -0400

    add configs

---
+++ Source/Remnant.Target.cs
@@ -0,0 +1,12 @@
+﻿using UnrealBuildTool;
+
+public class RemnantTarget : TargetRules {
+	public RemnantTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		ExtraModuleNames.AddRange(new string[] {
+			"ClothingSystemRuntime",
+			"GunfireRuntime",
+			"Remnant",
+		});
+	}
+}
